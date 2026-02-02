0d98c354d414b845a294cdd8201987c4f7adf827
master-jedii <pemakon.vlr@gmail.com>
2026-02-02 10:59:19 +0700

    setup react router with main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MainLayout from "./layouts/MainLayout"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <MainLayout>
-      <Home />
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,4 @@
+// src/pages/Login.tsx
+export default function Login() {
+  return <h1>Login Page</h1>
+}
