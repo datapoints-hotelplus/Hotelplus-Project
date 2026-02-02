5ac53775650745f93d0dd4a6062d7b16529db784
purimwwp <163145395+Purimppswt@users.noreply.github.com>
2026-02-02 17:44:52 +0700

    Login style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
+import "../src/styles/global.css"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* หน้า Login ใช้ AuthLayout */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        {/* หน้าเว็บหลักใช้ MainLayout */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Login/login.tsx
@@ -0,0 +1,28 @@
+import "./login.css"; 
+import logo from "../../assets/logo/Hotelplus-logo.jpg"
+
+export default function Login() {
+  return (
+    <div className="login-page">
+      <div className="login-card">
+        <img src={logo} alt="Hotel Plus" className="login-logo" />
+
+        <h2 className="login-title">เข้าสู่ระบบ</h2>
+
+        <label>อีเมล</label>
+        <input
+          type="email"
+          placeholder=""
+        />
+
+        <label>รหัสผ่าน</label>
+        <input
+          type="password"
+          placeholder=""
+        />
+
+        <button className="login-button">Log in</button>
+      </div>
+    </div>
+  );
+}
