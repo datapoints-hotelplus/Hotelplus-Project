a3a5788387f11cac9af19655fe552345bff6e549
purimwwp <163145395+Purimppswt@users.noreply.github.com>
2026-02-05 16:09:58 +0700

    403page

--- src/pages/Forbidden/Forbidden.tsx
+++ src/pages/Forbidden/Forbidden.tsx
@@ -1,3 +1,22 @@
-export default function Forbidden() {
-  return <h1>403 - ไม่มีสิทธิ์</h1>;
+import Footer from "../../layouts/Footer/Footer"
+import Navbar from "../../layouts/Navbar/Navbar"
+import './Forbidden.css'
+
+
+const Forbidden = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <div className="container-403">
+            <div className="box-403">
+                <h1>403 ไม่มีสิทธิ์เข้า</h1>
+                
+            </div>
+           
+        </div>
+        <Footer></Footer>
+    </div>
+  )
 }
+
+export default Forbidden
