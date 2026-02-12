fb0f95fd80962b5bfc1573f534c541d3a159fb09
master-jedii <pemakon.vlr@gmail.com>
2026-02-13 00:41:05 +0700

    test 11

--- client/src/pages/Online-revenue-mangemanet/ORM-Lite-Calculator/logic/fullPricing/getFullTier.ts
+++ client/src/pages/Online-revenue-mangemanet/ORM-Lite-Calculator/logic/fullPricing/getFullTier.ts
@@ -1,15 +1,15 @@
-import type{ FullTier } from "../../model/fullPricing.types";
+import type { FullTier } from "../../model/pricing.types";
 
 export function getFullTier(
-  averageRevenue: number
+  avgRevenuePerMonth: number
 ): FullTier {
 
-  if (averageRevenue < 120000) return "NONE";
-  if (averageRevenue <= 200000) return "F2";
-  if (averageRevenue <= 350000) return "F3";
-  if (averageRevenue <= 600000) return "F4";
-  if (averageRevenue <= 1000000) return "F5";
-  if (averageRevenue <= 1500000) return "F6";
-  if (averageRevenue <= 2500000) return "F7";
+  if (avgRevenuePerMonth < 120000) return "NONE";
+  if (avgRevenuePerMonth < 200000) return "F2";
+  if (avgRevenuePerMonth < 350000) return "F3";
+  if (avgRevenuePerMonth < 600000) return "F4";
+  if (avgRevenuePerMonth < 1000000) return "F5";
+  if (avgRevenuePerMonth < 1500000) return "F6";
+  if (avgRevenuePerMonth < 2500000) return "F7";
   return "F8";
 }
